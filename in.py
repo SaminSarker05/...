dd96aa1cb28957e8c1dd954a8d61caa8236af427
Samin Sarker <saminsarker@Samins-MBP.lan>
2021-12-26 19:38:44 -0500

    initial add

---
+++ in.py
@@ -0,0 +1,67 @@
+from tkinter import *
+
+root = Tk()
+
+root.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+from tkinter import *
+from tkinter import ttk
+
+root = Tk();
+#root.title("Feet to Meters")
+
+def click():
+    label = Label(root, text = "Hello " + e.get())
+    label.pack()
+
+e = Entry(root, width = 50)
+e.insert(0, "Username")
+#borderwidth
+#Entry.get() to text from input
+#e.insert(index, "") - default vale for textbox
+e.pack()
+
+b = Button(root, text = "Click", padx = 50, command = click)
+#fg = "", bg = ""
+b.pack()
+#main = ttk.Frame(root, padding = "10 10 10 10").grid(row = 0, column = 0)
+
+#b = Button(root, text = "press", command = root.destroy)
+#b.pack()
+"""
+
+
+
+
+
+root.mainloop()
