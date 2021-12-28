3902ac880ee6ce2e3e5bf4bc23a142211b04ee9c
Samin Sarker <saminsarker@Samins-MBP.lan>
2021-12-27 22:34:04 -0500

    numbers and buttons added for calculator

--- in.py
+++ in.py
@@ -2,6 +2,44 @@ from tkinter import *
 
 root = Tk()
 
+root.title("Calculator")
+
+#root.geometry("235x295")
+root.geometry("500x500")
+e = Entry(root).grid(row = 0, column = 0, columnspan = 4, padx = 10, pady = 0)
+#e.pack()
+
+num1 = Button(root, text = "1", padx = 20, pady = 20).grid(row = 4, column = 1)
+num2 = Button(root, text = "2", padx = 20, pady = 20).grid(row = 4, column = 2)
+num3 = Button(root, text = "3", padx = 20, pady = 20).grid(row = 4, column = 3)
+num4 = Button(root, text = "4", padx = 20, pady = 20).grid(row = 3, column = 1)
+num5 = Button(root, text = "5", padx = 20, pady = 20).grid(row = 3, column = 2)
+num6 = Button(root, text = "6", padx = 20, pady = 20).grid(row = 3, column = 3)
+num7 = Button(root, text = "7", padx = 20, pady = 20).grid(row = 2, column = 1)
+num8 = Button(root, text = "8", padx = 20, pady = 20).grid(row = 2, column = 2)
+num9 = Button(root, text = "9", padx = 20, pady = 20).grid(row = 2, column = 3)
+num0 = Button(root, text = "0", padx = 48, pady = 20).grid(row = 5, column = 1, columnspan = 2)
+
+deci = Button(root, text = ".", padx = 22, pady = 20).grid(row = 5, column = 3)
+
+clear = Button(root, text = "AC", padx = 20, pady = 20).grid(row = 1, column = 1)
+neg_pos = Button(root, text = "+/-", padx = 20, pady = 20).grid(row = 1, column = 2)
+percent = Button(root, text = "%", padx = 20, pady = 20).grid(row = 1, column = 3)
+
+
+
+div = Button(root, text = "/", padx = 20, pady = 20).grid(row = 1, column = 4)
+times = Button(root, text = "*", padx = 20, pady = 20).grid(row = 2, column = 4)
+minus = Button(root, text = "-", padx = 20, pady = 20).grid(row = 3, column = 4)
+add = Button(root, text = "+", padx = 20, pady = 20).grid(row = 4, column = 4)
+equal = Button(root, text = "=", padx = 20, pady = 20).grid(row = 5, column = 4)
+
+
+
+
+
+
+
 root.mainloop()
 
 
@@ -58,10 +96,8 @@ b.pack()
 
 #b = Button(root, text = "press", command = root.destroy)
 #b.pack()
-"""
-
-
-
 
 
 root.mainloop()
+"""
+
